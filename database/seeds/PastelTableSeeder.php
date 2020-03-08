84c06eb2d5d9f26349f9adca557ed70e86b284d1
RRJussiani <rr.jussiani@gmail.com>
2020-03-08 12:41:00 -0300

    Pastel: Adicionado imagem dos pasteis e alteração da seeder pastel inserindo as imagens.

--- database/seeds/PastelTableSeeder.php
+++ database/seeds/PastelTableSeeder.php
@@ -14,10 +14,11 @@ class PastelTableSeeder extends Seeder
     public function run()
     {
         $pasteis = [
-            ['nome' => 'Pastel de carne', 'preco' => '8.00'],
-            ['nome' => 'Pastel de frango', 'preco' => '8.50'],
-            ['nome' => 'Pastel de queijo', 'preco' => '8.00'],
-            ['nome' => 'Pastel de pizza', 'preco' => '9.00']
+            ['nome' => 'Pastel de carne', 'preco' => '8.00', 'foto' => 'pasteis/carne.jpg'],
+            ['nome' => 'Pastel de frango', 'preco' => '8.50', 'foto' => 'pasteis/frango.jpg'],
+            ['nome' => 'Pastel de queijo', 'preco' => '8.00', 'foto' => 'pasteis/queijo.jpg'],
+            ['nome' => 'Pastel de pizza', 'preco' => '9.00', 'foto' => 'pasteis/pizza.jpg'],
+            ['nome' => 'Pastel de camarão', 'preco' => '11.00', 'foto' => 'pasteis/camarao.jpg']
         ];
 
         foreach($pasteis as $pastel){
